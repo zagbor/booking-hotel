f460d68172fad74d1e134fa5f57f8c229d7a1646
bori.zagray <bori.zagray@x5.ru>
2025-10-27 09:04:13 +0200

    6

--- booking-service/src/main/java/com/zagbor/bookingservice/controller/BookingController.java
+++ booking-service/src/main/java/com/zagbor/bookingservice/controller/BookingController.java
@@ -8,7 +8,6 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
